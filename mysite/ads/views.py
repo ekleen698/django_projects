c611084899075ff96e6e6f82f57b492bee47b77a
Eric Kleen <eric.kleen@gmail.com>
2024-10-05 22:30:04 -0400

    Lesson4 Module2

--- mysite/ads/views.py
+++ mysite/ads/views.py
@@ -1,28 +1,99 @@
-from .models import Ad
-from .owner import OwnerListView, OwnerDetailView, OwnerCreateView, OwnerUpdateView, OwnerDeleteView
+from django.views import View
+from django.shortcuts import render, redirect, get_object_or_404
+from django.urls import reverse_lazy, reverse
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.http import HttpResponse
+
+from .models import Ad, Comment
+from .owner import OwnerListView, OwnerDetailView, OwnerDeleteView
+from .forms import CreateForm, CommentForm
 
 
 class AdListView(OwnerListView):
     model = Ad
-    # By convention:
-    # template_name = "ads/ad_list.html"
-
 
 class AdDetailView(OwnerDetailView):
     model = Ad
+    template_name = "ads/ad_detail.html"
+    def get(self, request, *args, **kw) :
+        ad = get_object_or_404(Ad, id=kw["pk"])
+        comments = Comment.objects.filter(ad=ad).order_by('-updated_at')
+        comment_form = CommentForm()
+        context = { 'ad' : ad, 'comments': comments, 'comment_form': comment_form }
+        return render(request, self.template_name, context)
 
-class AdCreateView(OwnerCreateView):
-    model = Ad
-    # List the fields to copy from the Article model to the Article form
-    fields = ['title', 'price', 'text']
+class AdCreateView(LoginRequiredMixin, View):
+    template_name = 'ads/ad_form.html'
+    success_url = ""
 
-class AdUpdateView(OwnerUpdateView):
-    model = Ad
-    fields = ['title', 'price', 'text']
-    # This would make more sense
-    # fields_exclude = ['owner', 'created_at', 'updated_at']
+    def get(self, request, pk=None):
+        form = CreateForm()
+        ctx = {'form': form}
+        return render(request, self.template_name, ctx)
 
+    def post(self, request, pk=None):
+        form = CreateForm(request.POST, request.FILES or None)
+
+        if not form.is_valid():
+            ctx = {'form': form}
+            return render(request, self.template_name, ctx)
+
+        # Add owner to the model before saving
+        ad = form.save(commit=False)
+        ad.owner = self.request.user
+        ad.save()
+
+        self.success_url = reverse_lazy('ads:ad_detail', args=[pk])
+        return redirect(self.success_url)
+
+class AdUpdateView(LoginRequiredMixin, View):
+    template_name = 'ads/ad_form.html'
+    success_url = ""
+
+    def get(self, request, pk):
+        ad = get_object_or_404(Ad, id=pk, owner=self.request.user)
+        form = CreateForm(instance=ad)
+        ctx = {'form': form}
+        return render(request, self.template_name, ctx)
+
+    def post(self, request, pk=None):
+        ad = get_object_or_404(Ad, id=pk, owner=self.request.user)
+        form = CreateForm(request.POST, request.FILES or None, instance=ad)
+
+        if not form.is_valid():
+            ctx = {'form': form}
+            return render(request, self.template_name, ctx)
+
+        ad = form.save(commit=False)
+        ad.save()
+
+        self.success_url = reverse_lazy('ads:ad_detail', args=[pk])
+        return redirect(self.success_url)
 
 class AdDeleteView(OwnerDeleteView):
     model = Ad
+    success_url = reverse_lazy('ads:ad_all')
 
+def stream_file(request, pk):
+    ad = get_object_or_404(Ad, id=pk)
+    response = HttpResponse()
+    response['Content-Type'] = ad.content_type
+    response['Content-Length'] = len(ad.picture)
+    response.write(ad.picture)
+    return response
+
+
+class CommentCreateView(LoginRequiredMixin, View):
+    def post(self, request, pk) :
+        ad = get_object_or_404(Ad, id=pk)
+        comment = Comment(text=request.POST['comment'], owner=request.user, ad=ad)
+        comment.save()
+        return redirect(reverse('ads:ad_detail', args=[pk]))
+
+class CommentDeleteView(OwnerDeleteView):
+    model = Comment
+    template_name = "ads/comment_delete.html"
+
+    def get_success_url(self):
+        pk = self.object.ad.id
+        return reverse('ads:ad_detail', args=[pk])
